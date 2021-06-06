3bd4a6ddf51f091ff67e400bb36c29e3d38d90ab
Fabio Abreu <fabiofiqueli@hotmail.com>
2021-06-06 18:17:33 +0100

    Criação da tabela pessoas

---
+++ app/src/main/java/pt/ipg/covid/TabelaPessoas.kt
@@ -0,0 +1,53 @@
+package pt.ipg.covid
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaPessoas(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+
+    fun cria(){
+        db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_DATA_TESTE  DATE NOT NULL, $CAMPO_RESULTADO TEXT NOT NULL, $CAMPO_NUM_UTENTE TEXT NOT NULL, $CAMPO_NOME TEXT NOT NULL, $CAMPO_SEXO TEXT NOT NULL, $CAMPO_DATA_NASCIMENTO DATE NOT NULL, $CAMPO_NUM_TELEMOVEL TEXT NOT NULL, $CAMPO_EMAIL TEXT NOT NULL, $CAMPO_MORADA TEXT NOT NULL, $CAMPO_ID_MEDICO INTEGER NOT NULL, $CAMPO_ID_UNIDADE_HOSPITALAR INTEGER NOT NULL, FOREIGN KEY ($CAMPO_ID_MEDICO) REFERENCES ${TabelaMedicos.NOME_TABELA}, FOREIGN KEY ($CAMPO_ID_UNIDADE_HOSPITALAR) REFERENCES ${TabelaUnidadeHospitalar.NOME_TABELA})")
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA,null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA,whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "Pessoas"
+        const val CAMPO_DATA_TESTE = "Data_do_Teste"
+        const val CAMPO_RESULTADO = "Resultado"
+        const val CAMPO_NUM_UTENTE = "Numero_de_Utente"
+        const val CAMPO_NOME = "Nome"
+        const val CAMPO_SEXO = "Sexo"
+        const val CAMPO_DATA_NASCIMENTO = "Data_de_Nascimento"
+        const val CAMPO_NUM_TELEMOVEL = "Telemovel"
+        const val CAMPO_EMAIL = "Email"
+        const val CAMPO_MORADA = "Morada"
+        const val CAMPO_ID_MEDICO = "Id_Medico"
+        const val CAMPO_ID_UNIDADE_HOSPITALAR = "Id_Unidade_Hospitalar"
+
+    }
+}
